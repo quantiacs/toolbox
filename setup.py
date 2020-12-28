68e6d93230791f1189aa8bedf98b0d5e9641591c
Dmitry Golovin <golovin@quantnet.ai>
2020-12-28 17:55:43 +0300

    minor improvement

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     license='MIT',
     packages=['qnt', 'qnt.ta', 'qnt.data'],
     package_data={'qnt': ['*.ipynb']},
-    install_requires=['xarray', 'pandas', 'numpy', 'scipy', 'tabulate', 'bottleneck', 'numba']
+    install_requires=['xarray', 'pandas', 'numpy', 'scipy', 'tabulate', 'bottleneck', 'numba', 'progressbar2']
 )
