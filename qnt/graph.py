a1c5fcdd77beac8e07b6fbd9671d4efd949284ce
Dmitry Golovin <golovin@quantnet.ai>
2021-04-08 19:04:30 +0300

    colab support

--- qnt/graph.py
+++ qnt/graph.py
@@ -12,15 +12,11 @@ def is_interact():
 def is_notebook():
     try:
         from IPython import get_ipython
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True   # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except:
-        return False # Probably standard Python interpreter
+        if 'IPKernelApp' not in get_ipython().config:
+            return False
+    except ImportError:
+        return False
+    return True
 
 
 if is_notebook():
