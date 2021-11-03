16603aead6a763f0e76f8a350d4fefa541239518
v.burdinskiy <burdinskiy@quantnet.ai>
2021-11-03 18:00:54 +0300

    pandas version update

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="qnt",
-    version="0.0.266",
+    version="0.0.267",
     url="https://quantiacs.com",
     license='MIT',
     packages=['qnt', 'qnt.ta', 'qnt.data', 'qnt.examples'],
     package_data={'qnt': ['*.ipynb']},
     install_requires=[
         'scipy>=1.4.1',
-        'pandas>=1.0.1',
+        'pandas>=1.2.5',
         'xarray>=0.16.0',
         'numpy>=1.18',
         'tabulate>=0.8.3',
