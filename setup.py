bc407abad7446238b3c985ab87757f84b5bf3016
v.burdinskiy <burdinskiy@quantnet.ai>
2022-06-02 12:34:41 +0300

    fix deprecated api

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="qnt",
-    version="0.0.269",
+    version="0.0.271",
     url="https://quantiacs.com",
     license='MIT',
     packages=['qnt', 'qnt.ta', 'qnt.data', 'qnt.examples'],
