2b192ff22799f177f6dfe860de7fe7cb71e7d3e0
burdinskiy-quantnet <burdinskiy@quantnet.ai>
2023-01-23 12:31:53 +0300

    precheck adapted for nasdaq100

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="qnt",
-    version="0.0.272",
+    version="0.0.273",
     url="https://quantiacs.com",
     license='MIT',
     packages=['qnt', 'qnt.ta', 'qnt.data', 'qnt.examples'],
