b27f65ebc575ce252dc0bafb8a5846b9b4ef3039
ugquant <uros.galic@teletrader.com>
2024-06-11 10:40:48 +0200

    Update exits.py
    - Compressed open_long and open_short into open_price
    - Compressed counter_long and counter_short into counter
    - Added last_atr function argument
    - Refactored multiple xr.where statements for improved clarity
    - Changed some variable names for improved clarity

--- qnt/exits.py
+++ qnt/exits.py
@@ -4,129 +4,107 @@ import qnt.data as qndata
 import qnt.ta as qnta
 
 #ATR-based take profit
-def take_profit(data, weights, state, threshold, long = True):
-    atr14 = qnta.atr(data.sel(field='high'), data.sel(field='low'), data.sel(field='close'), 14).isel(time=-1)
+def take_profit(data, weights, state, last_atr, threshold, long = True):
     keep_pos = [1] * len(data.isel(time=-1).asset) # Initialize positions to keep as 1 (hold)
-    last_pos = weights.isel(time=-1).values
+    curr_pos = weights.isel(time=-1).values
     prev_pos = weights.isel(time=-2).values
-    last_bar = data.isel(time=-1)
-        
-    open_prev_long = state['open_price_long']
-    open_prev_short = state['open_price_short']
-
-    if long == True:
-        # Update open price for long positions
-        open_price_long = xr.where(last_pos > 0, xr.where(prev_pos <= 0, last_bar.sel(field='open'), open_prev_long), np.nan)
-        # Determine if positions should be kept based on take profit threshold
-        keep_pos = xr.where(np.isnan(open_price_long), 1, (keep_pos * (last_bar.sel(field='close') < open_price_long + threshold * atr14)))
-        state['open_price_long'] = open_price_long
-        return keep_pos
+    last_open = data.isel(time=-1).sel(field='open')
+    last_close = data.isel(time=-1).sel(field='close')
+    open_prev = state['open_price']
+    
+    open_price = xr.where(curr_pos == 0, np.nan, last_open)
+    open_price = xr.where(curr_pos * prev_pos <= 0, open_price, open_prev)
+    
+    if long:
+        stop_condition = xr.where(curr_pos > 0, last_close < open_price + threshold * last_atr, 1)
     else:
-        # Update open price for short positions
-        open_price_short = xr.where(last_pos < 0, xr.where(prev_pos >= 0, last_bar.sel(field='open'), open_prev_short), np.nan)
-        # Determine if positions should be kept based on take profit threshold
-        keep_pos = xr.where(np.isnan(open_price_short), 1, (keep_pos * (last_bar.sel(field='close') > open_price_short - threshold * atr14)))
-        state['open_price_short'] = open_price_short
-        return keep_pos
+        stop_condition = xr.where(curr_pos < 0, last_close > open_price - threshold * last_atr, 1)  
+        
+    keep_pos = xr.where(np.isnan(open_price), 1, stop_condition)       
+    state['open_price'] = open_price
+    return keep_pos
 
 #ATR-based stop loss
-def stop_loss(data, weights, state, threshold, long = True):
-    atr14 = qnta.atr(data.sel(field='high'), data.sel(field='low'), data.sel(field='close'), 14).isel(time=-1)
+def stop_loss(data, weights, state, last_atr, threshold, long = True):
     keep_pos = [1] * len(data.isel(time=-1).asset) # Initialize positions to keep as 1 (hold)
-    last_pos = weights.isel(time=-1)
+    curr_pos = weights.isel(time=-1)
     prev_pos = weights.isel(time=-2)
-    last_bar = data.isel(time=-1)
+    last_open = data.isel(time=-1).sel(field='open')
+    last_close = data.isel(time=-1).sel(field='close')
+    open_prev = state['open_price']
     
-    open_prev_long = state['open_price_long']
-    open_prev_short = state['open_price_short']
-    
-    if long == True:
-        # Update open price for long positions
-        open_price_long = xr.where(last_pos > 0, xr.where(prev_pos <= 0, last_bar.sel(field='open'), open_prev_long), np.nan)
-        # Determine if positions should be kept based on stop loss threshold
-        keep_pos = xr.where(np.isnan(open_price_long), 1, (keep_pos * (last_bar.sel(field='close') > open_price_long - threshold * atr14)))
-        state['open_price_long'] = open_price_long
-        return keep_pos
+    open_price = xr.where(curr_pos == 0, np.nan, last_open)
+    open_price = xr.where(curr_pos * prev_pos <= 0, open_price, open_prev)
+
+    if long:
+        stop_condition = xr.where(curr_pos > 0, last_close > open_price - threshold * last_atr, 1)
     else:
-        # Update open price for short positions
-        open_price_short = xr.where(last_pos < 0, xr.where(prev_pos >= 0, last_bar.sel(field='open'), open_prev_short), np.nan)
-        # Determine if positions should be kept based on stop loss threshold
-        keep_pos = xr.where(np.isnan(open_price_short), 1, (keep_pos * (last_bar.sel(field='close') < open_price_short + threshold * atr14)))
-        state['open_price_short'] = open_price_short
-        return keep_pos
+        stop_condition = xr.where(curr_pos < 0, last_close < open_price + threshold * last_atr, 1)
+        
+    keep_pos = xr.where(np.isnan(open_price), 1, stop_condition)
+    state['open_price'] = open_price
+    return keep_pos
 
 #Percentage-based take profit
 def take_profit_percentage(data, weights, state, threshold, long = True):
     keep_pos = [1] * len(data.isel(time=-1).asset) # Initialize positions to keep as 1 (hold)
-    last_pos = weights.isel(time=-1).values
+    curr_pos = weights.isel(time=-1).values
     prev_pos = weights.isel(time=-2).values
-    last_bar = data.isel(time=-1)
-        
-    open_prev_long = state['open_price_long']
-    open_prev_short = state['open_price_short']
-
-    if long == True:
-        # Update open price for long positions
-        open_price_long = xr.where(last_pos > 0, xr.where(prev_pos <= 0, last_bar.sel(field='open'), open_prev_long), np.nan)
-        # Determine if positions should be kept based on take profit threshold
-        keep_pos = xr.where(np.isnan(open_price_long), 1, (keep_pos * (last_bar.sel(field='close') < open_price_long * (1+threshold/100))))
-        state['open_price_long'] = open_price_long
-        return keep_pos
+    last_open = data.isel(time=-1).sel(field='open')
+    last_close = data.isel(time=-1).sel(field='close')
+    open_prev = state['open_price']
+    
+    open_price = xr.where(curr_pos == 0, np.nan, last_open)
+    open_price = xr.where(curr_pos * prev_pos <= 0, open_price, open_prev)
+    
+    if long:
+        stop_condition = xr.where(curr_pos > 0, last_close < open_price * (1+threshold/100), 1)
     else:
-        # Update open price for short positions
-        open_price_short = xr.where(last_pos < 0, xr.where(prev_pos >= 0, last_bar.sel(field='open'), open_prev_short), np.nan)
-        # Determine if positions should be kept based on take profit threshold
-        keep_pos = xr.where(np.isnan(open_price_short), 1, (keep_pos * (last_bar.sel(field='close') > open_price_short * (1-threshold/100))))
-        state['open_price_short'] = open_price_short
-        return keep_pos
+        stop_condition = xr.where(curr_pos < 0, last_close > open_price * (1-threshold/100), 1)  
+        
+    keep_pos = xr.where(np.isnan(open_price), 1, stop_condition)       
+    state['open_price'] = open_price
+    return keep_pos
 
 #Percentage-based stop loss
 def stop_loss_percentage(data, weights, state, threshold, long = True):
     keep_pos = [1] * len(data.isel(time=-1).asset) # Initialize positions to keep as 1 (hold)
-    last_pos = weights.isel(time=-1)
+    curr_pos = weights.isel(time=-1)
     prev_pos = weights.isel(time=-2)
-    last_bar = data.isel(time=-1)
+    last_open = data.isel(time=-1).sel(field='open')
+    last_close = data.isel(time=-1).sel(field='close')
+    open_prev = state['open_price']
     
-    open_prev_long = state['open_price_long']
-    open_prev_short = state['open_price_short']
-    
-    if long == True:
-        # Update open price for long positions
-        open_price_long = xr.where(last_pos > 0, xr.where(prev_pos <= 0, last_bar.sel(field='open'), open_prev_long), np.nan)
-        # Determine if positions should be kept based on stop loss threshold
-        keep_pos = xr.where(np.isnan(open_price_long), 1, (keep_pos * (last_bar.sel(field='close') > open_price_long * (1-threshold/100))))
-        state['open_price_long'] = open_price_long
-        return keep_pos
+    open_price = xr.where(curr_pos == 0, np.nan, last_open)
+    open_price = xr.where(curr_pos * prev_pos <= 0, open_price, open_prev)
+
+    if long:
+        stop_condition = xr.where(curr_pos > 0, last_close > open_price * (1-threshold/100), 1)
     else:
-        # Update open price for short positions
-        open_price_short = xr.where(last_pos < 0, xr.where(prev_pos >= 0, last_bar.sel(field='open'), open_prev_short), np.nan)
-        # Determine if positions should be kept based on stop loss threshold
-        keep_pos = xr.where(np.isnan(open_price_short), 1, (keep_pos * (last_bar.sel(field='close') < open_price_short * (1+threshold/100))))
-        state['open_price_short'] = open_price_short
-        return keep_pos    
+        stop_condition = xr.where(curr_pos < 0, last_close < open_price * (1+threshold/100), 1)
+        
+    keep_pos = xr.where(np.isnan(open_price), 1, stop_condition)
+    state['open_price'] = open_price
+    return keep_pos
     
 #Exit after a certain number of days
-def day_counter(data, weights, state, threshold, long = True):
-    atr14 = qnta.atr(data.sel(field='high'), data.sel(field='low'), data.sel(field='close'), 14).isel(time=-1)
+def day_counter(data, weights, state, days, long = True):
     keep_pos = [1] * len(data.isel(time=-1).asset) # Initialize positions to keep as 1 (hold)
-    last_pos = weights.isel(time=-1)
+    curr_pos = weights.isel(time=-1)
     prev_pos = weights.isel(time=-2)
-    last_bar = data.isel(time=-1)
 
-    counter_prev_long = state['counter_long']
-    counter_prev_short = state['counter_short']
-
-    if long == True:
-        # Update counter for long positions
-        counter_long = xr.where(last_pos > 0, counter_prev_long+1, 0)
-        keep_pos = (keep_pos * (counter_long < threshold)).astype(int)
-        counter_long = xr.where(counter_long > threshold, 0, counter_long)
-        state['counter_long'] = counter_long
-        return keep_pos
+    counter_prev = state['counter']
+    
+    reset_or_increase = xr.where(counter_prev > days, 0, counter_prev+1)
+        
+    if long:
+        counter = xr.where(curr_pos > 0, reset_or_increase, counter_prev) 
+        keep_pos = xr.where(curr_pos > 0, counter < days, 1)
     else:
-        # Update counter for short positions
-        counter_short = xr.where(last_pos < 0, counter_prev_short+1, 0)
-        keep_pos = (keep_pos * (counter_short < threshold)).astype(int)
-        counter_short = xr.where(counter_short > threshold, 0, counter_short)
-        state['counter_short'] = counter_short
-        return keep_pos
+        counter = xr.where(curr_pos < 0, reset_or_increase, counter_prev)        
+        keep_pos = xr.where(curr_pos < 0, counter < days, 1)
+        
+    counter = xr.where(curr_pos != 0, counter, 0)    
+    counter = xr.where(curr_pos * prev_pos < 0, 1, counter)
+    state['counter'] = counter
+    return keep_pos
